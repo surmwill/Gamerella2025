eb21d686761ed965f04989e77810a94a53292090
surmwill <william.surmak@gmail.com>
2025-11-15 15:43:24 -0500

    adding audio

--- Gamerella2025_Unity/Assets/Scripts/Game/ElfId.cs
+++ Gamerella2025_Unity/Assets/Scripts/Game/ElfId.cs
@@ -2,8 +2,8 @@
 {
     public enum ElfId
     {
-        Level0_Instrument_1 = 0,
-        Level0_Instrument_2 = 1,
-        Level0_Instrument_3 = 2,
+        Level0_Instrument0 = 0,
+        Level0_Instrument1 = 1,
+        Level0_Instrument2 = 2,
     }
 }
