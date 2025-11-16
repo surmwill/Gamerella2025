6eecd25e967cb55b71e26a04aca6e7b49c0be8f2
surmwill <william.surmak@gmail.com>
2025-11-16 12:06:39 -0500

    adding sound to each elf

--- Gamerella2025_Unity/Assets/Scripts/Game/ElfId.cs
+++ Gamerella2025_Unity/Assets/Scripts/Game/ElfId.cs
@@ -2,16 +2,21 @@
 {
     public enum ElfId
     {
-        Elf_Bell_01 = 0,
-        Elf_Bell_02 = 1,
-        Elf_Bell_03 = 2,
+        Elf_Level0_Bass = 0,
+        Elf_Level0_Drums = 1,
         
-        Elf_Drum_01 = 10,
-        Elf_Drum_02 = 11,
-        Elf_Drum_03 = 12,
+        Elf_Level1_Bass = 10,
+        Elf_Level1_Bells = 11,
+        Elf_Level1_Drums = 12,
+        Elf_Level1_Synthmelo = 13,
+        Elf_Level1_Vox = 14,
         
-        Elf_Synth_01 = 20,
-        Elf_Synth_02 = 21,
-        Elf_Synth_03 = 22,
+        Elf_Level2_Bass = 20,
+        Elf_Level2_Chordspad = 21,
+        Elf_Level2_Hat = 22,
+        Elf_Level2_Kick = 23,
+        Elf_Level2_Lead = 24,
+        Elf_Level2_MainChords = 25,
+        Elf_Level2_Snare = 26,
     }
 }
